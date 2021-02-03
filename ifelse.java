9423c68eeceefce38c71cfc96a02c29b4976974d
ghanshyamcode <ghanshyamv2kv@gmial.com>
2021-02-03 20:16:00 +0530

    use of ifelse

---
+++ ifelse.java
@@ -0,0 +1,24 @@
+public class ifelse {
+    public static void main(String args[]){
+        int month=4;
+        String season;
+
+        if(month == 12 || month == 1 || month == 2)
+           season="Winter";
+        else if(month == 3 || month == 4 || month == 5 )
+           season="spring";
+        else if (month == 6 || month == 7 || month == 8)
+           season="Summer";
+        else if(month == 9 || month == 10 || month == 11)   
+            season="Autumm";
+
+        else
+            season="Bogus month" ;   
+
+        System.out.println("April is in the "+season+".");
+            
+    
+    }
+
+    
+}
